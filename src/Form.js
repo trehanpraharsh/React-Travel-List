fdd11e8bd48460bdb6c6f937ab53244c09fdd3c0
trehanpraharsh <trehanpraharsh@gmail.com>
2023-08-04 21:58:27 +0530

    Section-6 Finished

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip 🥰 ?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
       <button>Add</button>
     </form>
   );
